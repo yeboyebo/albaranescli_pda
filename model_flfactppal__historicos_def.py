6511ae0521d3bb686825315356866117ac4d5644
xavikortes <javiercortesveliz@gmail.com>
2018-12-21 08:44:55 +0100

    Reducir duplicidad

--- model_flfactppal__historicos_def.py
+++ model_flfactppal__historicos_def.py
@@ -16,47 +16,11 @@ from YBLEGACY.constantes import *
 
 class albaranescli_pda(interna):
 
-    def albaranescli_pda_initValidation(self, name, data=None):
-        response = True
-        return response
-
-    def albaranescli_pda_iniciaValoresLabel(self, model=None, template=None, cursor=None):
-        labels = {}
-        return labels
-
-    def albaranescli_pda_bChLabel(self, fN=None, cursor=None):
-        labels = {}
-        return labels
-
-    def albaranescli_pda_getFilters(self, model, name, template=None):
-        filters = []
-        return filters
-
-    def albaranescli_pda_getForeignFields(self, model, template=None):
-        fields = []
-        return fields
-
     def albaranescli_pda_getDesc(self):
-        desc = None
-        return desc
+        return None
 
     def __init__(self, context=None):
-        super(albaranescli_pda, self).__init__(context)
-
-    def initValidation(self, name, data=None):
-        return self.ctx.albaranescli_pda_initValidation(name, data)
-
-    def iniciaValoresLabel(self, model=None, template=None, cursor=None):
-        return self.ctx.albaranescli_pda_iniciaValoresLabel(model, template, cursor)
-
-    def bChLabel(self, fN=None, cursor=None):
-        return self.ctx.albaranescli_pda_bChLabel(fN, cursor)
-
-    def getFilters(self, model, name, template=None):
-        return self.ctx.albaranescli_pda_getFilters(model, name, template)
-
-    def getForeignFields(self, model, template=None):
-        return self.ctx.albaranescli_pda_getForeignFields(model, template)
+        super().__init__(context)
 
     def getDesc(self):
         return self.ctx.albaranescli_pda_getDesc()
@@ -66,14 +30,14 @@ class albaranescli_pda(interna):
 class head(albaranescli_pda):
 
     def __init__(self, context=None):
-        super(head, self).__init__(context)
+        super().__init__(context)
 
 
 # @class_declaration ifaceCtx #
 class ifaceCtx(head):
 
     def __init__(self, context=None):
-        super(ifaceCtx, self).__init__(context)
+        super().__init__(context)
 
 
 # @class_declaration FormInternalObj #
--- model_flfacturac__registrosexportados.py
+++ model_flfacturac__registrosexportados.py
@@ -20,24 +20,6 @@ class albaranescli_pda_registrosexportados(interna_registrosexportados, helpers.
     class Meta:
         proxy = True
 
-    def initValidation(name, data=None):
-        return form.iface.initValidation(name, data)
-
-    def iniciaValoresLabel(self, template=None, cursor=None, data=None):
-        return form.iface.iniciaValoresLabel(self, template, cursor)
-
-    def bChLabel(fN=None, cursor=None):
-        return form.iface.bChLabel(fN, cursor)
-
-    def getFilters(self, name, template=None):
-        return form.iface.getFilters(self, name, template)
-
-    def getForeignFields(self, template=None):
-        return form.iface.getForeignFields(self, template)
-
-    def getDesc():
-        return form.iface.getDesc()
-
 
 # @class_declaration registrosexportados #
 class registrosexportados(albaranescli_pda_registrosexportados, helpers.MixinConAcciones):
@@ -45,6 +27,9 @@ class registrosexportados(albaranescli_pda_registrosexportados, helpers.MixinCon
 
     class Meta:
         proxy = True
+
+    def getIface(self=None):
+        return form.iface
 
 
 definitions = importlib.import_module("models.flfacturac.registrosexportados_def")
